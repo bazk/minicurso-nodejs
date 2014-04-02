f13e129b4b8eab8f0ff36e3194af87c5bad2a9ea
Eduardo L. Buratti <elb09@c3sl.ufpr.br>
2014-04-02 15:18:25 -0300

    Add snippets

--- js/main.js
+++ js/main.js
@@ -1,27 +1,42 @@
 angular.module('minicurso', ['ngRoute', 'hljs'])
     .config(function($routeProvider, $locationProvider) {
         $routeProvider
-            .when('/1', { templateUrl: 'partials/part1.html' })
-            .when('/2', { templateUrl: 'partials/part2.html' })
-            .when('/3', { templateUrl: 'partials/part3.html' })
+            .when('/:snippet', {
+                templateUrl: 'partials/snippets.html',
+                controller: 'SnippetCtrl'
+            })
             .otherwise({redirectTo: '/1'});
     })
 
-    .controller('MainCtrl', function($scope, $location) {
-        $scope.partNo = null;
+    .provider('Snippets', function () {
+        var snippets = [
+            'snippets/node-install.html',
+            'snippets/node-install-prebuilt.html'
+        ];
 
-        $scope.$on("$routeChangeSuccess", function (event, current, previous) {
-            $scope.prev = null;
-            $scope.next = null;
+        this.$get = function () {
+            return snippets;
+        }
+    })
 
-            $scope.partNo = parseInt($location.path().slice(1), 10);
+    .controller('SnippetCtrl', function($scope, $location, $routeParams, Snippets) {
+        $scope.snippets = Snippets;
 
-            if ($scope.partNo > 1)
-                $scope.prev = $scope.partNo - 1;
+        if (!parseInt($routeParams.snippet, 10)) {
+            $location.path('/1');
+        }
 
-            if ($scope.partNo < 2)
-                $scope.next = $scope.partNo + 1;
-        });
+        $scope.curSnippetNo = parseInt($routeParams.snippet, 10);
+        $scope.curSnippet = $scope.snippets[$scope.curSnippetNo-1];
+
+        $scope.prev = null;
+        $scope.next = null;
+
+        if ($scope.curSnippetNo > 1)
+            $scope.prev = $scope.curSnippetNo - 1;
+
+        if ($scope.curSnippetNo < $scope.snippets.length)
+            $scope.next = $scope.curSnippetNo + 1;
 
         $scope.goPrev = function () {
             $location.path('/'+$scope.prev);
